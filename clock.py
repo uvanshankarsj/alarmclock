4d75c7f9b4c07c7ac9fad536a4c16ad5c21abeec
uvanshankarsj <84233610+uvanshankarsj@users.noreply.github.com>
2023-06-07 19:10:28 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from playsound import playsound
+import time
+import os
+
+now = datetime.now()
+
+alarm=str(input())
+while now.strftime("%H:%M:%S")!=alarm:
+    now = datetime.now()
+    print(now.strftime("%H:%M:%S"))
+    time.sleep(1)
+file='Venpura-StarMusiQ.Fun.mp3'
+# os.system("vlc " + file)
+playsound('Venpura-StarMusiQ.Fun.mp3')
